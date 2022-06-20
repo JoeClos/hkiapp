6cd77beb3079f6d7195e109e818a513114278142
Josephine <josephine.closan@yahoo.com>
2022-06-20 18:21:16 +0300

    change package json for deployment

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ function requireHTTPS(req, res, next) {
 }
 const express = require('express');
 const app = express();
-const app = express();
+// const app = express();
 app.use(requireHTTPS);
 
 app.use(express.static('./dist/hkiapp'));
 
-app.get('/*', function(req, res) {
+app.get('/*', function(_req, res) {
     res.sendFile('index.html', {root: 'dist/hkiapp/'}
   );
   });
 
-  app.listen(process.env.PORT || 8080);
+  app.listen(process.env.PORT || 4200);
